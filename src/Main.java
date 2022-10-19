cfc0272c7b1e3317a236e3100a621a0ef3329198
Бегали <b_mashrapov@mail.ru>
2022-10-19 17:16:02 +0200

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        int a = 1234567890;
+        byte b = -108;
+        short c = 1256;
+        long d = 123456789;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом int равно " + d);
+
+    }
+}
