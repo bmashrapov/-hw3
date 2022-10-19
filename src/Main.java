cc4fca212c8338e4c4359a317a580e5b7e1d8ad1
Бегали <b_mashrapov@mail.ru>
2022-10-19 17:54:03 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,41 @@ public class Main {
         int a = 1234567890;
         byte b = -108;
         short c = 1256;
-        long d = 123456789;
+        long d = 123456789125L;
+        float q = 3.15f;
+        double p = 3.145646464;
         System.out.println("Значение переменной a с типом int равно " + a);
         System.out.println("Значение переменной b с типом byte равно " + b);
         System.out.println("Значение переменной c с типом short равно " + c);
         System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной q с типом float равно " + q);
+        System.out.println("Значение переменной p с типом double равно " + p);
+        //Задача 2
+        float f = 27.12F;
+        long g = 987678965459L;
+        double h = 2.786;
+        boolean k = f > 25;
+        short j = 569;
+        short l = -159;
+        short m = 27897;
+        byte o = 67;
+        //Часть 2
+        byte a1 = 9;
+        short b1 = 156;
+        int c1 = -2145645;
+        long d1 = 3547854545445454L;
+        float f1 = 5.67f;
+        double g1 = 3.1456556;
+        char h1 = 35;
+        boolean k1 = a < 10;
+        System.out.println(k1);
+        System.out.println(h1);
+
+
+
+
+
+
 
     }
 }
